47454daf1b424344a1969c3d9bdf28007707454d
NidhishVyas <vyasnidhish2001@gmail.com>
2022-02-24 21:05:29 +0530

    Rules UI

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-let navlist = document.querySelector('.navList');
-let openicon = document.querySelector('.open');
-let closeicon = document.querySelector('#cross');
+let navlist = document.querySelector(".navList");
+let openicon = document.querySelector(".open");
+let closeicon = document.querySelector("#cross");
 
+openicon.addEventListener("click", show);
 
+function show() {
+  // navlist.style.display = 'flex';
+  navlist.style.left = "0";
+}
 
-openicon.addEventListener('click' , show)
-    
- function show(){
-    // navlist.style.display = 'flex';
-    navlist.style.left = '0';
- }
+closeicon.addEventListener("click", shut);
 
-
- closeicon.addEventListener('click' , shut)
-
- function shut(){
-    navlist.style.left = '-100%';
- }
+function shut() {
+  navlist.style.left = "-100%";
+}
